dd62bf2e75fd6756205870e889465dee1f2f262d
Felipe <felipemarostega.ff@gmail.com>
2025-06-24 18:22:46 -0300

    chore: removing unecessary prints

--- pythia/plugins/nasap/__init__.py
+++ pythia/plugins/nasap/__init__.py
@@ -1,4 +1,3 @@
-import time
 import urllib.request
 from pythia.plugins.nasap.grid import id_at
 from pythia.functions import xy_from_list, xy_from_vector
@@ -9,15 +8,10 @@ def initialize(args, plugins, config):
     plugins = register_plugin_function(PluginHook.post_config, ev_load_config, config, plugins)
     return plugins
 
-# kwargs = stores the event data, and the function must return the event data; I can modify kwargs
 
-# TO-DO:
-# download files for all sites
-
-def ev_load_config(*args, **kwargs): #always return kwargs on the functions
+def ev_load_config(*args, **kwargs):
     config = kwargs.get("full_config")
     visited = set([])
-    print("rodando ---")
     for run in config.get("runs", []):
         wsta = run["wsta"] 
         if isinstance(run["sites"], list):
@@ -25,20 +19,17 @@ def ev_load_config(*args, **kwargs): #always return kwargs on the functions
         else:
             sites = xy_from_vector(run["sites"])
         for site in sites:
-            print(site)
-            nasap_id = id_at(site[0], site[1]) #lookup_wth(wsta, site[0], site[1])
+            nasap_id = id_at(site[0], site[1]) 
             if nasap_id in visited:
-                print(f"visitado --- {nasap_id}")
                 continue
             visited.add(nasap_id)
-            print(f"baixando --- {nasap_id}")
 
             fetch_data('20010101', '20020101', site[0], site[1], f"wth/{nasap_id}.WTH");
 
     return kwargs
 
 
-def fetch_data(start_date: str, end_date: str, longitude: float, latitude: float, output_path: str): #output must be on the pattern wth/id +.WTH
+def fetch_data(start_date: str, end_date: str, longitude: float, latitude: float, output_path: str):
     
     base_url = "https://power.larc.nasa.gov/api/temporal/daily/point"
     community = "AG"     
@@ -60,13 +51,11 @@ def fetch_data(start_date: str, end_date: str, longitude: float, latitude: float
     urllib.request.urlretrieve(url, output_path)
 
 def lookup_wth(args, lat, lng):
-    now = time.time()
     args = args.split("::")[1:]
     cell_id = None
     if "vector" in args:
         idx = args.index("vector")
         cell_id = find_closest_vector_coords(args[idx + 1], lng, lat, args[idx + 2])
-    print(f"execution time: {time.time() - now}")
     return cell_id
 
 
